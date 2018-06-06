0188818d723d2251050b3848c9c3d7c019420f1f
alexkaufman06 <alexkaufman06@gmail.com>
2018-06-06 16:04:16 -0700

    Add more shapes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
 function setup() {
-  createCanvas(400,400);
+  createCanvas(640,640,WEBGL);
+  print('this works');
+  console.log('or does it!?');
 }
 
 function draw() {
   background(0);
-  ellipse(200, 200, 50, 50);
+  ellipse(-200,-200,50,50);
+  rect(50,50,100,100);
+  rotateX(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  cylinder(20,50);
 }
+
+// NEXT VIDEO: 1.4: Color - p5.js Tutorial
