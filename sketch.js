7ba971c4249c2a42ca64378ed5cc8b710f41ca8d
alexkaufman06 <alexkaufman06@gmail.com>
2018-06-07 12:26:45 -0700

    Add color functions

--- sketch.js
+++ sketch.js
@@ -6,11 +6,17 @@ function setup() {
 
 function draw() {
   background(0);
+  stroke(255,0,0);
+  fill(0,0,255);
   ellipse(-200,-200,50,50);
+  stroke(0,255,0);
+  fill(255,0,0);
   rect(50,50,100,100);
+  stroke(0,0,255);
+  fill(0,255,0,50); //last value is transparency
   rotateX(frameCount * 0.01);
   rotateZ(frameCount * 0.01);
   cylinder(20,50);
 }
 
-// NEXT VIDEO: 1.4: Color - p5.js Tutorial
+// NEXT VIDEO: How to upload your sketch - p5.js Tutorial
